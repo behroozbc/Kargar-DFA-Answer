46b3efc93f5b3415ab6bf9c17a21210b2959dd2f
behrooz bozorg chami <behroozbozorgchami@hotmail.com>
2021-11-12 22:18:09 +0330

    not dfa converted answer

--- Answer-2/Program.cs
+++ Answer-2/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using static System.Console;
+using System.Linq;
 
-namespace Answer_2
+Write("Input String : ");
+var dfa_input = ReadLine().ToCharArray().ToList();
+if (dfa_input.Count != 4)
 {
-    class Program
+    WriteLine("InValid Input");
+    return;
+}
+var IsSafe = true;
+var result = true;
+for (int i = 1; i < dfa_input.Count(); i++)
+{
+    if (dfa_input[i] == '1')
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        IsSafe = true;
+        continue;
+    }
+    if (dfa_input[i - 1] == '0' && dfa_input[i] == '0' && IsSafe)
+    {
+        IsSafe = false;
+        continue;
+    }
+    if(IsSafe==false)
+    {
+        result = false;
+        break;
     }
 }
+if (result == false)
+    WriteLine("Input is not valid");
+else
+    WriteLine("Input is Valid");
